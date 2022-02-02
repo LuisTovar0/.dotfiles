416976b47e0192fa00d8e0c045527616cd6eb6ea
Luís Tovar <1181585@isep.ipp.pt>
2022-02-02 14:06:51 +0000

    Trying to fix the fish_variables problem

--- setup.sh
+++ setup.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
-then
-	echo Installation will be done assuming this repo is located in the user home directory.
-	folder=".."
-else
-	folder="/home/$1"
-	echo Installation will be done on the home directory $folder.
-fi
-
-# update and upgrade packages
-sudo apt update
-yes | sudo apt upgrade # yes outputs "y" to all the inputs of the piped command
-
-# fish; fish config depencencies
-yes | sudo apt install fish
-yes | sudo apt install lolcat
-yes | sudo apt install neofetch
-
-echo
-echo "🗿🗿🗿 Finished installing dependencies 🗿🗿🗿"
-echo
-
 echo_ex() {
 	echo "$@"
 	sh -c "$@"
 }
 
-# creating links for the config files
-echo_ex "ln -s $PWD/.vimrc $folder/.vimrc"
-echo_ex "ln -s $PWD/.gitconfig $folder/.gitconfig"
 echo_ex "mkdir $folder/.config/"
 echo_ex "mkdir $folder/.config/fish/"
 echo_ex "mkdir $folder/.config/fish/functions/"
+
+# update and upgrade packages
+echo_ex "sudo apt update"
+echo_ex "yes | sudo apt upgrade" # yes outputs "y" to all the inputs of the piped command
+
+# fish; fish config depencencies
+echo_ex "yes | sudo apt install fish"
+echo_ex "yes | sudo apt install lolcat"
+echo_ex "yes | sudo apt install neofetch"
+
+echo
+echo "🗿🗿🗿 Finished installing dependencies 🗿🗿🗿"
+echo
+
+# creating links for the config files
+folder="/home/$USER"
+echo_ex "ln -s $PWD/.vimrc $folder/.vimrc"
+echo_ex "ln -s $PWD/.gitconfig $folder/.gitconfig"
 echo_ex "ln -s $PWD/fish/config.fish $folder/.config/fish/config.fish"
 echo_ex "ln -s $PWD/fish/fish_prompt.fish $folder/.config/fish/functions/fish_prompt.fish"
